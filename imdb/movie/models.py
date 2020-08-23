2e23a0609bb7e0365fcb9c5c8db08313e87a9f95
kanishk5111 <kanishk5111@users.noreply.github.com>
2020-08-23 14:34:22 +0530

    First commit

---
+++ imdb/movie/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+
+CATEGORY_CHOICES =  (
+    ('action','ACTION'),
+    ('drama','DRAMA'),
+    ('comedy','COMEDY'),
+    ('romance','ROMANCE'),
+)
+
+# Create your models here.
+
+LANGUAGE_CHOICES =  (
+    ('english','ENGLISH'),
+    ('hindi','HINDI'),
+)
+
+STATUS_CHOICES =  (
+    ('RA','RECENTLY ADDED'),
+    ('MW','MOST WATCHED'),
+    ('TR','TOP RATED'),
+
+)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    image= models.ImageField(upload_to='movies')
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=10)
+    language=models.CharField(choices=LANGUAGE_CHOICES,max_length=10)
+    status=models.CharField(choices=STATUS_CHOICES,max_length=2)
+    cast=models.CharField(max_length=100)
+
+    year_of_production= models.DateField()
+    views_count=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+LINK_CHOICES =  (
+    ('D','DOWNLOAD LINK'),
+    ('W','WATCH LINK'),
+
+)
+
+class MovieLinks(models.Model):
+    movie =  models.ForeignKey(Movie,related_name='movie_watch_link',on_delete=models.CASCADE)
+    type = models.CharField(choices=LINK_CHOICES,max_length=1)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.movie
+
---
+++ imdb/movie/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView,DetailView
+from django.views.generic.dates import YearArchiveView
+
+from  .models import Movie,MovieLinks
+
+class MovieList(ListView):
+    model = Movie
+    paginate_by = 1
+
+
+class MovieDetail(DetailView):
+    model = Movie
+
+    def get_object(self):
+        object=super(MovieDetail,self).get_object()
+        object.views_count += 1
+        object.save()
+        return object
+
+    def get_context_data(self, **kwargs):
+        context=super(MovieDetail,self).get_context_data(**kwargs)
+        context['links'] = MovieLinks.objects.filter(movie=self.get_object())
+        return context
+
+class MovieCategory(ListView):
+    model = Movie
+
+
+    def get_queryset(self):
+        self.category = self.kwargs['category']
+        return Movie.objects.filter(category =self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieCategory,self).get_context_data(**kwargs)
+        context['movie_category'] = self.category
+        return  context
+
+
+class MovieLanguage(ListView):
+    model = Movie
+    paginate_by = 1
+
+
+    def get_queryset(self):
+        self.language = self.kwargs['lang']
+        return Movie.objects.filter(language =self.language)
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieLanguage,self).get_context_data(**kwargs)
+        context['movie_language'] = self.language
+        return  context
+
+
+
+class MovieSearch(ListView):
+    model = Movie
+    paginate_by = 1
+
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+        if query:
+            object_list = self.model.objects.filter(title__icontains=query)
+            # print(query)
+            # print(object_list)
+        else:
+            object_list = self.model.objects.none()
+        return object_list
+
+class MovieYear(YearArchiveView):
+    queryset = Movie.objects.all()
+    date_field = 'year_of_production'
+    make_object_list = True
+    allow_future = True
+
+    print(queryset)
